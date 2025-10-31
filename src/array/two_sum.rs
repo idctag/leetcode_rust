2d1edca9c7e4bef228e935a88e0e0e4724aea377
Ochir Battulga <ochir.b@denode.io>
2025-10-31 16:36:44 +0800

    two_sum

--- src/array/two_sum.rs
+++ src/array/two_sum.rs
@@ -1,5 +1,15 @@
 //https://leetcode.com/problems/two-sum
-pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {}
+pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    use std::collections::HashMap;
+    let mut values: HashMap<i32, i32> = HashMap::new();
+    for (i, v) in nums.iter().enumerate() {
+        match values.get(&(target - v)) {
+            Some(index) => return vec![*index, i as i32],
+            None => values.insert(*v, i as i32),
+        };
+    }
+    vec![]
+}
 
 #[cfg(test)]
 mod tests {
