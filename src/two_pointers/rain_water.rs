43ca0bdb82e9821b6a9c77a26911c13754667e04
Ochir Battulga <ochir.b@denode.io>
2025-11-25 12:09:06 +0800

    rain water

--- src/two_pointers/rain_water.rs
+++ src/two_pointers/rain_water.rs
@@ -1,6 +1,21 @@
 pub fn trap(height: Vec<i32>) -> i32 {
-    let (mut l, mut r, mut total) = (0, height.len() - 1, 0);
-    total
+    if height.is_empty() {
+        return 0;
+    }
+    let (mut l, mut r, mut res) = (0, height.len() - 1, 0);
+    let (mut l_max, mut r_max) = (height[l], height[r]);
+    while l < r {
+        if l_max < r_max {
+            l += 1;
+            l_max = l_max.max(height[l]);
+            res += l_max - height[l];
+        } else {
+            r -= 1;
+            r_max = r_max.max(height[r]);
+            res += r_max - height[r]
+        }
+    }
+    res
 }
 
 #[cfg(test)]
