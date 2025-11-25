65f1ca2d79bcd0c277102316b2f1b10f9e3af2e2
Ochir Battulga <ochir.b@denode.io>
2025-11-25 12:55:23 +0800

    minstack

---
+++ src/stack/min_stack.rs
@@ -0,0 +1,36 @@
+struct MinStack {
+    stack: Vec<i32>,
+    min_stack: Vec<i32>,
+}
+
+impl MinStack {
+    fn new() -> Self {
+        Self {
+            stack: Vec::new(),
+            min_stack: Vec::new(),
+        }
+    }
+
+    fn push(&mut self, val: i32) {
+        self.stack.push(val);
+        if self.min_stack.is_empty() || val <= *self.min_stack.last().unwrap() {
+            self.min_stack.push(val);
+        }
+    }
+
+    fn pop(&mut self) {
+        if let Some(top) = self.stack.pop() {
+            if top == *self.min_stack.last().unwrap() {
+                self.min_stack.pop();
+            }
+        }
+    }
+
+    fn top(&self) -> i32 {
+        *self.stack.last().unwrap()
+    }
+
+    fn get_min(&self) -> i32 {
+        *self.min_stack.last().unwrap()
+    }
+}
