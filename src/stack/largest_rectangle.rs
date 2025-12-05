34f09db99236d6949e23c6b2f4b1d76fb7e9bfe1
Ochir Battulga <ochir.b@denode.io>
2025-12-05 14:49:33 +0800

    largest rectangle setup

---
+++ src/stack/largest_rectangle.rs
@@ -0,0 +1,6 @@
+pub fn solution(heights: Vec<i32>) -> i32 {
+    let mut ans = 0;
+    let mut stack: Vec<i32> = Vec::new();
+    for (i, h) in heights.into_iter().enumerate() {}
+    ans
+}
--- src/stack/mod.rs
+++ src/stack/mod.rs
@@ -1,5 +1,6 @@
 pub mod car_fleet;
 pub mod daily_temperatures;
 pub mod eval_reverse;
+pub mod largest_rectangle;
 pub mod min_stack;
 pub mod valid_parentheses;
