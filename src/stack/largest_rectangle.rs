01b1b7ee90bfe068b2f7a2f08799230d43c3acda
Ochir Battulga <ochir.b@denode.io>
2025-12-11 16:40:08 +0800

    largets rectangle setup

--- src/stack/largest_rectangle.rs
+++ src/stack/largest_rectangle.rs
@@ -1,7 +1,23 @@
-pub fn solution(_heights: Vec<i32>) -> i32 {
-    // let mut ans = 0;
-    // let mut stack: Vec<i32> = Vec::new();
-    // for (i, h) in heights.into_iter().enumerate() {}
-    // ans
-    0
+pub fn solution(heights: Vec<i32>) -> i32 {
+    let mut ans = 0;
+    let mut stack: Vec<i32> = Vec::new();
+    for (i, h) in heights.into_iter().enumerate() {}
+    ans
+}
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_one() {
+        let input = vec![2, 1, 5, 6, 2, 3];
+        assert_eq!(solution(input), 10)
+    }
+    #[test]
+    fn case_two() {
+        let input = vec![2, 4];
+        assert_eq!(solution(input), 4)
+    }
 }
