7089bad9c1f6e79d10a32ff4243e583b5f350f54
Ochir Battulga <ochir.b@denode.io>
2025-12-12 17:55:04 +0800

    largest rectangle in progress

--- src/stack/largest_rectangle.rs
+++ src/stack/largest_rectangle.rs
@@ -1,8 +1,21 @@
 pub fn solution(heights: Vec<i32>) -> i32 {
-    let mut ans = 0;
-    let mut stack: Vec<i32> = Vec::new();
-    for (i, h) in heights.into_iter().enumerate() {}
-    ans
+    let mut max_area = 0;
+    let len: i32 = heights.len() as i32;
+    let mut stack: Vec<(i32, i32)> = Vec::new();
+    for (i, h) in heights.into_iter().enumerate() {
+        if stack.is_empty() {
+            stack.push((i as i32, h));
+        }
+        let top = stack.last().unwrap();
+        if h < top.1 {
+        } else {
+        }
+    }
+    for p in &stack {
+        let area = ((len - p.0) + 1) * p.1;
+        max_area = max_area.max(area)
+    }
+    max_area
 }
 
 #[cfg(test)]
